ce69c93b0ac2c7dbf36cded4c2fe177b6126bd5a
md-umai <mdumair786114@gmail.com>
2025-09-16 02:30:07 +0530

    Add more list of people data

--- list-search/script.js
+++ list-search/script.js
@@ -3,20 +3,33 @@ let userContainer = document.querySelector('.userContainer');
 const arr = [
     {
         profileUrl:'https://images.squarespace-cdn.com/content/v1/6204821bfe06b76898b431c5/80221678-0539-4495-8007-0096677e1eca/image00016.jpeg?format=2500w" alt="User Image',
-        name:'John Doe',
-        email:'johndoe@gmail.com'
+        name:'Mohammed Umair',
+        email:'mdumair@gmail.com'
     },
 
     {
         profileUrl:'https://images.squarespace-cdn.com/content/v1/6204821bfe06b76898b431c5/1644529076537-44RQ7BGS0ZHA5ZC9X991/EE7A1239.jpg" alt="User Image',
-        name:'lorem2',
-        email:'lorem2@gmail.com'
+        name:'Omer Sayeed Quadri',
+        email:'quadrisaab@gmail.com'
     },
 
     {
         profileUrl:'https://www.apetogentleman.com/wp-content/uploads/2018/06/top-male-models.jpg" alt="User Image',
-        name:'Tyson Beckford',
-        email:'tysonbeckford@gmail.com'
+        name:'Ziad Shaik',
+        email:'ziadshaik@gmail.com'
+    }
+
+    {
+        profileUrl:'https://www.apetogentleman.com/wp-content/uploads/2018/06/top-male-models.jpg" alt="User Image',
+        name:'Khaja Hamza',
+        email:'hamza@gmail.com'
+    }
+
+
+    {
+        profileUrl:'https://www.apetogentleman.com/wp-content/uploads/2018/06/top-male-models.jpg" alt="User Image',
+        name:'Mohammed Mudassir Hussain Quadri',
+        email:'mudassir@gmail.com'
     }
 ];
 
